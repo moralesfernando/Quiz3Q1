727ac2ef35ee0f9f244486d6ab019f224a5e3f18
moral <moral@DESKTOP-TPSHLG8.fios-router.home>
2016-10-09 17:00:38 -0400

    4 test

--- Quiz3maven/src/test/java/TestTriangle.java
+++ Quiz3maven/src/test/java/TestTriangle.java
@@ -25,7 +25,7 @@ public class TestTriangle {
 	}
 
 	@Test
-	public void AreaTest() {
+	public void AreaTest1() {
 		Triangle testTri = new Triangle(3,4,5);
 		double expectedArea = 6.0;
 		double actualArea = testTri.getArea();
@@ -33,11 +33,62 @@ public class TestTriangle {
 	}
 	
 	@Test
-	public void PerimeterTest() {
+	public void AreaTest2() {
+		Triangle testTri = new Triangle(3,3,5);
+		double expectedArea = 4.146;
+		double actualArea = testTri.getArea();
+		assertEquals(expectedArea,actualArea,0.001);	
+	}
+	
+	@Test
+	public void AreaTest3() {
+		Triangle testTri = new Triangle(4,4,5);
+		double expectedArea = 7.806;
+		double actualArea = testTri.getArea();
+		assertEquals(expectedArea,actualArea,0.001);	
+	}
+	
+	@Test
+	public void AreaTest4() {
+		Triangle testTri = new Triangle(5,5,7);
+		double expectedArea = 12.497;
+		double actualArea = testTri.getArea();
+		assertEquals(expectedArea,actualArea,0.001);	
+	}
+	
+	@Test
+	public void PerimeterTest1() {
 		Triangle testTri = new Triangle(3,4,5);
 		double expectedPer = 12.0;
 		double actualPer = testTri.getPerimeter();
 		assertEquals(expectedPer,actualPer,0.001);
 	}
+	
+	
+	@Test
+	public void PerimeterTest2() {
+		Triangle testTri = new Triangle(3,3,5);
+		double expectedPer = 11.0;
+		double actualPer = testTri.getPerimeter();
+		assertEquals(expectedPer,actualPer,0.001);
+	}
+
+	@Test
+	public void PerimeterTest3() {
+		Triangle testTri = new Triangle(4,4,5);
+		double expectedPer = 13.0;
+		double actualPer = testTri.getPerimeter();
+		assertEquals(expectedPer,actualPer,0.001);
+	}
+
+	@Test
+	public void PerimeterTest4() {
+		Triangle testTri = new Triangle(5,5,7);
+		double expectedPer = 17.0;
+		double actualPer = testTri.getPerimeter();
+		assertEquals(expectedPer,actualPer,0.001);
+	}
+
+	
 
 }
